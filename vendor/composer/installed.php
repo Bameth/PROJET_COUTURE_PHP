14fc29ecf07959d3beae5aa80a2b82f319a07ac7
Ameth Ba <amethba8826@gmail.com>
2025-10-12 20:50:14 +0000

    `push`

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ab/couture',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd7a5fc97f2b2dd94d475d7dd4ecfc15033e1ba22',
+        'type' => 'projet',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ab/couture' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd7a5fc97f2b2dd94d475d7dd4ecfc15033e1ba22',
+            'type' => 'projet',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
